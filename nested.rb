70f304b64a3574d8ef981caedb7437782c084319
Lemrabt Boudadya <lboudadya@gmail.com>
2019-01-11 23:52:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,11 @@ def adding_matz
         }
      }
   #programmer_hash = {:yukihiro_matsumoto[:known_for] = "Ruby"}
-  #programmer_hash[:yukihiro_matsumoto][:languages] = "LISP", "C"
+  #programmer_hash[:yukihiro_matsumoto][:languages] = "LISP", "
+  #programmer_hash[:yukihiro_matsumoto] = {
+  known_for: "Ruby",
+  languages: ["Zelda", "Tigerlily", "Frenchie"]
+  }
     
 end
 
